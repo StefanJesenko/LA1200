f264f00303fbe8bdff65f60c3f3bb32641eea1c1
StefanJesenko <110892250+StefanJesenko@users.noreply.github.com>
2022-11-01 10:26:39 +0100

    Funktion Teamnamen hinzugefügt.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace LA_1200
+﻿
+namespace LA_1200
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           
+            bool loop = true;
+            List<string> listenWorkshopNamen = new List<string>();
+               WorkshopNamen workshopNamen = new();
+           while(loop == true)
+            {
+                string workshopName = workshopNamen.Workshop(); 
+                if(workshopName == "n")
+                {
+                    break;
+                }
+                listenWorkshopNamen.Add(workshopName);
                 
+           }
+
+            foreach (string s in listenWorkshopNamen)
+            {
+                Console.WriteLine(s);
+            }
         }
-    }
+    }  
 }
---
+++ WorkshopNamen.cs
@@ -0,0 +1,20 @@
+﻿namespace LA_1200
+{
+    class WorkshopNamen
+    {
+        public string Workshop()
+        {
+            
+            Console.Write("Geben Sie einen Namen ein: ");
+            string workshopNamen = Console.ReadLine();
+
+
+
+
+
+
+            return workshopNamen;
+        }
+    }
+    
+}
