5c65420b6c15be0755f1d83843a85fdd570f4876
Kurizaki <110892283+Kurizaki@users.noreply.github.com>
2022-11-08 10:52:51 +0100

    Update and rename WorkSopEinteilung.cs to WorkShopEinteilung.cs

--- WorkShopEinteilung.cs
+++ WorkShopEinteilung.cs
@@ -4,53 +4,53 @@ namespace WhorkshopEinteilung
     {
         public static void Main(string[] args)
         {
-            List<string> workshop = new();
-            List<string> workshopTeil = new();
+            List<string> Workshops = new();
+            List<string> Teilnehmer = new();
             List<string> workshopausw = new();
-            List<int> workshopauswprio = new();
-            int mindanz = workshop.Count * 4;
+            List<int> priorität = new();
+            int[] prioVor = new int[3];
+            int mindanz = Workshops.Count * 4;
             bool control = false;
             bool check = false;
-            int z = 1;
+            int z = 0;
             do
             {
                 do
                 {
                     Console.WriteLine("Geben Sie den Vor- und Nachnamen eines Workshopteilnehmers ein.");
-                    workshopTeil.Add(Convert.ToString(Console.ReadLine()));
-
-                    for (int i = -1; i <= 3;)
+                    Teilnehmer.Add(Console.ReadLine());
+                    for (int i = 0; i < 3; i++)
                     {
+                        z = 0;
+                        z++;
                         Console.WriteLine("Geben Sie ein welcher Workshop Sie besuchen wollen.");
-                        workshopausw.Add(Convert.ToString(Console.ReadLine()));
+                        workshopausw.Add(Console.ReadLine());
                         do
                         {
-                            i++;
                             try
                             {
+                                control = true;
                                 Console.WriteLine("Geben Sie ein welche Priorität Workshop {0} hat (1-3)", z);
-                                workshopauswprio.Add(Convert.ToInt32(Console.ReadLine()));
+                                prioVor[i] = Convert.ToInt32(Console.ReadLine());
                             }
                             catch
                             {
                                 Console.WriteLine("Ungültige Eingabe");
                                 control = false;
-                                i--;
                             }
-                            if (workshopauswprio[i] > 3 || workshopauswprio[i] < 1)
+                            if (prioVor[i] < 1 || prioVor[i] > 3)
                             {
                                 Console.WriteLine("Bitte geben sie nur eine Zahl zwischen 1-3 ein.");
                                 control = false;
-                                i--;
                             }
                             else
                             {
-                                z++;
                                 control = true;
+                                priorität.Add(prioVor[i]);
                             }
                         } while (control == false);
                     }
-                } while (mindanz == workshopTeil.Count);
+                } while (mindanz == Teilnehmer.Count);
                 Console.WriteLine("Um weiter zu fahren drücken Sie (n) Ansonst geben Sie weitere Teilnehmern ein.");
                 string eingabe = Console.ReadLine();
                 if (eingabe != "n")
