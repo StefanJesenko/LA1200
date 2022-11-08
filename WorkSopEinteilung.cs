8023a31d75e94186345d7a4865c2dbd3db94c638
Kurizaki <110892283+Kurizaki@users.noreply.github.com>
2022-11-08 09:29:32 +0100

    Create WorkSopEinteilung.cs

---
+++ WorkSopEinteilung.cs
@@ -0,0 +1,60 @@
+namespace WhorkshopEinteilung
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> workshop = new();
+            List<string> workshopTeil = new();
+            List<string> workshopausw = new();
+            List<int> workshopauswprio = new();
+            int mindanz = workshop.Count * 4;
+            bool control = false;
+            bool check = false;
+            int z = 1;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Geben Sie den Vor- und Nachnamen eines Workshopteilnehmers ein.");
+                    workshopTeil.Add(Convert.ToString(Console.ReadLine()));
+
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        Console.WriteLine("Geben Sie ein welcher Workshop Sie besuchen wollen.");
+                        workshopausw.Add(Convert.ToString(Console.ReadLine()));
+                        do
+                        {
+                            try
+                            {
+                                Console.WriteLine("Geben Sie ein welche Priorität Workshop {0} hat (1-3)", z);
+                                workshopauswprio.Add(Convert.ToInt32(Console.ReadLine()));
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ungültige Eingabe");
+                                control = false;
+                            }
+                            if (workshopauswprio[i] > 3 || workshopauswprio[i] < 1)
+                            {
+                                Console.WriteLine("Bitte geben sie nur eine Zahl zwischen 1-3 ein.");
+                                control = false;
+                            }
+                            else
+                            {
+                                z++;
+                                control = true;
+                            }
+                        } while (control == false);
+                    }
+                } while (mindanz == workshopTeil.Count);
+                Console.WriteLine("Um weiter zu fahren drücken Sie (n) Ansonst geben Sie weitere Teilnehmern ein.");
+                string eingabe = Console.ReadLine();
+                if (eingabe != "n")
+                {
+                    check = true;
+                }
+            } while (check == true);
+        }
+    }
+}
