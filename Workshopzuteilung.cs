4f3f93febd3f895e2d4fdffa9fd8767298b54359
Stefan <110892250+StefanJesenko@users.noreply.github.com>
2022-11-15 10:29:09 +0100

    Update Workshopzuteilung.cs

--- Workshopzuteilung.cs
+++ Workshopzuteilung.cs
@@ -1,25 +1,25 @@
 ﻿
 namespace LA_1200
 {
-    class Workshopzuteilung
+    class WorkshopAllication
     {
         public int k = 0;
         public int i = 0;
         public List<int> kCount = new List<int>();
-        public List<string> Workshopzuteilungen(List<string> participants, List<int> priority, List<string> workshop)
+        public List<string> WorkshopAllications(List<string> participants, List<int> priority, List<string> workshop)
         {
             List<string> allocation  = new List<string>();
             List<string> finalAllocation = new List<string>();
             int priorityCount = 3;
             int numberWorkshop = 0;
-            Workshopzuteilung workshopzuteilung = new();
+            WorkshopAllication workshopAllication = new();
              
             
              int j = 0;
             
             for( int y = 0; y < workshop.Count; y++)
             {
-                workshopzuteilung.WorkshopzuteilungAlgorythmus(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j,i,k);
+                workshopAllication.WorkshopAllicationAlgorythm(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j,i,k);
                 i = 0;
                 i++;
                 k = 0;
@@ -43,7 +43,7 @@ namespace LA_1200
                 {
                     if (kCount[z] != 4)
                     {
-                        workshopzuteilung.WorkshopzuteilungAlgorythmus(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j, i, k);
+                        workshopAllication.WorkshopAllicationAlgorythm(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j, i, k);
                     }
                     else
                     {
@@ -70,7 +70,7 @@ namespace LA_1200
                 {
                     if(kCount[z] != 4)
                     {
-                        workshopzuteilung.WorkshopzuteilungAlgorythmus(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j, i, k);
+                        workshopAllication.WorkshopAllicationAlgorythm(participants, priority, workshop, allocation, priorityCount, numberWorkshop, j, i, k);
                     }
                     else
                     {
@@ -97,7 +97,7 @@ namespace LA_1200
             return finalAllocation;
         }
         
-        public List<string> WorkshopzuteilungAlgorythmus(List<string> participants, List<int> priority, List<string>workshop, List<string> Allocation, int priorityCount, int numberWorkshop, int j, int i, int k)
+        public List<string> WorkshopAllicationAlgorythm(List<string> participants, List<int> priority, List<string>workshop, List<string> Allocation, int priorityCount, int numberWorkshop, int j, int i, int k)
         {
 
            
